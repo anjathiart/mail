0be59abb93ea32bae935ae17c2b31a2b746b1c82
Anja Thiart <anjathiart@gmail.com>
2020-09-15 22:44:18 +0200

    WIP read email view and archive / unarchive

--- mail/static/mail/inbox.jsx
+++ mail/static/mail/inbox.jsx
@@ -36,23 +36,23 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  // Show the mailbox and hide other views
-  ReactDOM.unmountComponentAtNode(document.getElementById('emails-view'));
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+	// Show the mailbox and hide other views
+	ReactDOM.unmountComponentAtNode(document.getElementById('emails-view'));
+	document.querySelector('#emails-view').style.display = 'block';
+	document.querySelector('#compose-view').style.display = 'none';
 
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+	// Show the mailbox name
+	document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  fetch(`/emails/${mailbox}`)
+	fetch(`/emails/${mailbox}`)
 		.then(response => response.json())
 		.then(emails => {
-    // Print emails
-    console.log(emails);
+		// Print emails
+		console.log(emails);
 
-    // ... do something else with emails ...
-    renderEmailsView(emails)
-});
+		// ... do something else with emails ...
+		renderEmailsView(emails)
+	});
 }
 
 function send_email() {
@@ -81,6 +81,63 @@ function send_email() {
 	});
 }
 
+function view_email(email) {
+	// Show the mailbox and hide other views
+	ReactDOM.unmountComponentAtNode(document.getElementById('emails-view'));
+	document.querySelector('#emails-view').style.display = 'none';
+	document.querySelector('#compose-view').style.display = 'none';
+	document.querySelector('#readEmail-view').style.display = 'block';
+
+	console.log(email)
+
+	const archiveButtonText = email.archived ? 'Un-Archive' : 'Archive'
+
+	class ReadEmail extends React.Component {
+
+		render() {
+			return (
+				<div className="readEmail" id="email.id">
+					<div className="readEmail__header">
+						<p><strong>From: </strong>{ email.sender }</p>
+						<p><strong>To: </strong>{ email.recipients }</p>
+						<p><strong>Subject: </strong>{ email.subject }</p>
+						<p><strong>Timestamp: </strong>{ email.timestamp }</p>
+					</div>
+					<div className="readEmail__action">
+						<button className="btn btn-primary" id="replyButton">Reply</button>
+						<button className="btn btn-primary" id="archiveButton">{archiveButtonText}</button>
+					</div>
+					<hr/>
+					<p className="readEmail__body">{ email.body }</p>
+        		</div>
+			);
+		}
+	}
+
+	ReactDOM.render(<ReadEmail />, document.querySelector("#readEmail-view"));
+	document.querySelector('.readEmail').addEventListener('click', (event) => {
+		if (event.target.id === "replyButton") {
+			console.log('REPLY TODO');
+		} else if (event.target.id === 'archiveButton') {
+			fetch(`/emails/${email.id}`, {
+				method: 'PUT',
+				body: JSON.stringify({
+					archived: !email.archived
+				})
+			})
+			.then(result => {
+			    // Print result
+			    console.log(result);
+			})
+			.catch((error) => {
+				console.error('Error', error)
+			});
+		}
+	})
+
+
+}
+
 function renderEmailsView(emails) {
 
 	ReactDOM.unmountComponentAtNode(document.getElementById('emails-view'));
@@ -89,10 +146,10 @@ function renderEmailsView(emails) {
 
 		render() {
 			return (
-				<div className="emailCard">
-					<p>{this.props.sender}!</p>
-					<p>{this.props.subject}!</p>
-					<p>{this.props.timestamp}!</p>
+				<div className="emailCard" id={"email" + this.props.index} >
+					<p>{this.props.sender}</p>
+					<p>{this.props.subject}</p>
+					<p>{this.props.timestamp}</p>
         		</div>
 			);
 		}
@@ -100,7 +157,7 @@ function renderEmailsView(emails) {
 
 	let list = [];
 	for (let i = 0; i < emails.length; i += 1) {
-		list.push(<EmailCard sender={emails[i].sender} subject={emails[i].subject} timestamp={emails[i].timestamp} />)
+		list.push(<EmailCard sender={emails[i].sender} subject={emails[i].subject} timestamp={emails[i].timestamp} index={i} />)
 	}
 
 	class EmailList extends React.Component {
@@ -114,7 +171,16 @@ function renderEmailsView(emails) {
 	}
 
 	ReactDOM.render(<EmailList />, document.querySelector("#emails-view"));
+
+	document.querySelectorAll('.emailCard').forEach(el => {
+		el.addEventListener('click', (event) => {
+			console.log(el.id.replace('email', ''))
+			let emailIndex = el.id.replace('email', '')
+			view_email(emails[emailIndex])
+		})
+	})
 }
+
 
 
 
